ee70fa6fad3cc6f733f8ebcaa3ff228e2badb6d8
breathxavier <ruveydaturkkan1703@gmail.com>
2020-08-24 14:37:56 +0200

    github 3. deneme new branch

--- src/test/java/githubdeneme01/Class01.java
+++ src/test/java/githubdeneme01/Class01.java
@@ -4,6 +4,7 @@ public class Class01 {
     public static void main(String[] args) {
         System.out.println("Github 1. denemeden merhaba");
         System.out.println("Github 2. denemeden merhaba");
+        System.out.println("Github 3. deneme, branch olusturduk");
     }
 
 
