b3381e81508998dbe84512e7d4d92d8bd839ff44
breathxavier <67017795+breathxavier@users.noreply.github.com>
2020-08-24 14:41:55 +0200

    Update Class01.java

--- src/test/java/githubdeneme01/Class01.java
+++ src/test/java/githubdeneme01/Class01.java
@@ -4,6 +4,8 @@ public class Class01 {
     public static void main(String[] args) {
         System.out.println("Github 1. denemeden merhaba");
         System.out.println("Github 2. denemeden merhaba");
+        
+         System.out.println("Github 4. deneme remote repo dan locale");
     }
 
 
