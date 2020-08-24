1aa675ba8be061bbbb66ae351f8671f97faa9eb2
breathxavier <ruveydaturkkan1703@gmail.com>
2020-08-24 14:35:24 +0200

    github denemeden 2. commit

--- src/test/java/githubdeneme01/Class01.java
+++ src/test/java/githubdeneme01/Class01.java
@@ -3,6 +3,7 @@ package githubdeneme01;
 public class Class01 {
     public static void main(String[] args) {
         System.out.println("Github 1. denemeden merhaba");
+        System.out.println("Github 2. denemeden merhaba");
     }
 
 
